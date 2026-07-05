d684ff3c74bd8e035d4137d12e32aad3f2f048ca
BorisStehnii <bstegniy@gmail.com>
2026-07-05 18:34:36 +0300

    esp32 mod2.4 #4

--- ESP32_2.4_Interrupts/src/main.cpp
+++ ESP32_2.4_Interrupts/src/main.cpp
@@ -2,84 +2,181 @@
 
 #define BUTTON_PIN 2
 #define DEBOUNCE_TIME 50
-
+#define POLL_INTERVAL 5
 
 int counter = 0;
 
-volatile bool change_flag = false;
-volatile bool press_handling_flag = false; // флаг на запуск обробки натискання
-volatile bool debounce_flag = false; // флаг на запуск дебоунс
-volatile bool tempering_flag = false; // флаг на відпускання
+unsigned long lastPollTime = 0;
+unsigned long debounceStart = 0;
 
+enum class ButtonState
+{
+  Released,
+  DebouncePress,
+  Pressed,
+  DebounceRelease
+};
+bool buttonPressed = false;
 
+ButtonState buttonState = ButtonState::Released;
 
-unsigned long last = 0;
-unsigned long debounce_Start = 0; // момент натискання кнопки
-unsigned long tempering_Start = 0; //момент відпускання
+void setup()
+{
+  pinMode(BUTTON_PIN, INPUT);
 
-
-
-void IRAM_ATTR ISR_button(){
-  change_flag = true;
+  Serial.begin(115200);
 }
 
-void setup() {
-  pinMode(BUTTON_PIN, INPUT); 
-  Serial.begin(115200); 
-  attachInterrupt(BUTTON_PIN, ISR_button, FALLING); // апаратне переривання
+void loop()
+{
+  unsigned long now = millis();
+
+  if (now - lastPollTime >= POLL_INTERVAL)
+  {
+    lastPollTime = now;
+
+    buttonPressed = digitalRead(BUTTON_PIN) == LOW;
+  }
+
+  switch (buttonState)
+  {
+    case ButtonState::Released:
+      // Кнопка була відпущена, але ми побачили LOW
+      if (buttonPressed)
+      {
+        debounceStart = now;
+        buttonState = ButtonState::DebouncePress;
+      }
+      break;
+
+
+    case ButtonState::DebouncePress:
+      // Чекаємо 50 мс і перевіряємо, чи кнопка досі натиснута
+      if (now - debounceStart >= DEBOUNCE_TIME)
+      {
+        if (buttonPressed)
+        {
+          counter++;
+          Serial.printf("Counter %d\n", counter);
+
+          buttonState = ButtonState::Pressed;
+        }
+        else
+        {
+          // Це був брязкіт, повертаємось у стан відпущеної кнопки
+          buttonState = ButtonState::Released;
+        }
+      }
+      break;
+
+    case ButtonState::Pressed:
+      // Кнопка натиснута і вже зарахована, Тепер чекаємо початку відпускання
+      if (!buttonPressed)
+      {
+        debounceStart = now;
+        buttonState = ButtonState::DebounceRelease;
+      }
+      break;
+
+
+    case ButtonState::DebounceRelease:
+      // Чекаємо 50 мс і перевіряємо, чи кнопка досі відпущена
+      if (now - debounceStart >= DEBOUNCE_TIME)
+      {
+        if (!buttonPressed)
+        {
+          buttonState = ButtonState::Released;
+        }
+        else
+        {
+          // Це був брязкіт при відпусканні, кнопка все ще натиснута
+          buttonState = ButtonState::Pressed;
+        }
+      }
+      break;
+  }
 }
 
-void loop() {
+////////////////////////////////////////////////////////////////////////////
+// домашне 2.4 завдання 3
+// #define BUTTON_PIN 2
+// #define DEBOUNCE_TIME 50
+
+
+// int counter = 0;
+
+// volatile bool change_flag = false;
+// volatile bool press_handled_flag = false; // флаг на запуск обробки натискання
+// volatile bool debounce_flag = false; // флаг на запуск дебоунс
+// volatile bool release_flag = false; // флаг на відпускання
+
+
+// unsigned long debounce_Start = 0; // момент натискання кнопки
+// unsigned long release_Start = 0; //момент відпускання
+
+
+
+// void IRAM_ATTR ISR_button(){
+//   change_flag = true;
+// }
+
+// void setup() {
+//   pinMode(BUTTON_PIN, INPUT); 
+//   Serial.begin(115200); 
+//   attachInterrupt(BUTTON_PIN, ISR_button, FALLING); // апаратне переривання
+// }
+
+// void loop() {
   
-  unsigned long now = millis(); // зчитуємо ниниішній час
+//   unsigned long now = millis(); // зчитуємо ниниішній час
 
-  // перевіряємо флаг від апаратного переривання
-  if (change_flag){
+//   // перевіряємо флаг від апаратного переривання
+//   if (change_flag){
     
-    change_flag = false;
+//     change_flag = false;
 
-    if (!press_handling_flag && !debounce_flag){
-      debounce_Start = now;
-      debounce_flag = true;
-    }
-  }
+//     if (!press_handled_flag && !debounce_flag){
+//       debounce_Start = now;
+//       debounce_flag = true;
+//     }
+//   }
 
-  // перевірка реального стану за 50мс
-  if (debounce_flag && now - debounce_Start >= DEBOUNCE_TIME) {
-      debounce_flag = false;
+//   // перевірка реального стану за 50мс
+//   if (debounce_flag && now - debounce_Start >= DEBOUNCE_TIME) {
+//       debounce_flag = false;
 
-      if (digitalRead(BUTTON_PIN) == LOW){
+//       if (digitalRead(BUTTON_PIN) == LOW){
 
-        counter++;
-        press_handling_flag = true;
+//         counter++;
+//         press_handled_flag = true;
 
-        Serial.printf(" Counter %d \n", counter);
-      }
-  }
-  // очікуване відпускання 
-  if (press_handling_flag){
+//         Serial.printf(" Counter %d \n", counter);
+//       }
+//   }
+//   // очікуване відпускання. контролюємо відпускання перед очікуванням нового натискання. 
+//   if (press_handled_flag){
 
-    if (digitalRead(BUTTON_PIN) == HIGH){
+//     if (digitalRead(BUTTON_PIN) == HIGH){
 
-      if (!tempering_flag){
-        tempering_Start = now;
-        tempering_flag = true;
-      }
+//       if (!release_flag){
+//         release_Start = now;
+//         release_flag = true;
+//       }
 
-      // кнопка відпущенна стабільно 50мс
-      if (now - tempering_Start >= DEBOUNCE_TIME){
-        press_handling_flag = false;
-        tempering_flag = false;
-      }
+//       // кнопка відпущенна стабільно 50мс
+//       if (now - release_Start >= DEBOUNCE_TIME){
+//         press_handled_flag = false;
+//         release_flag = false;
+//       }
 
-    }
-    else{
-      // це був брязкіт
-      tempering_flag = false;
-    }
+//     }
+//     else{
+//       // це був брязкіт
+//       release_flag = false;
+//     }
 
-  }
-}
+//   }
+// }
 
 
 ////////////////////////////////////////////////////////////////////////////
